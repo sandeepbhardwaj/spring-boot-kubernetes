abbef0a1e36e90d018cc8c5142f3603f10d2a6b7
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2020-01-05 13:21:09 +0530

    Added log file

--- message-producer/src/main/java/com/koko/springboot/messageproducer/MessageProducerApplication.java
+++ message-producer/src/main/java/com/koko/springboot/messageproducer/MessageProducerApplication.java
@@ -1,13 +1,17 @@
 package com.koko.springboot.messageproducer;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MessageProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessageProducerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MessageProducerApplication.class, args);
+    }
+    
 }
