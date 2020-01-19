c476f6f22d3ee0a58889e0f2c3decf61ad3032a9
Sandeep Bhardwaj <sandeepbhardwaj01@gmail.com>
2020-01-19 11:38:58 +0530

    changes

--- message-producer/src/main/java/com/koko/springboot/messageproducer/MessageProducerApplication.java
+++ message-producer/src/main/java/com/koko/springboot/messageproducer/MessageProducerApplication.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MessageProducerApplication {
 
